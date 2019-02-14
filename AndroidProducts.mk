0d7624acdf8d0b1a64840823640e043199db8bf2
ramon17 <abdurahmon-99@mail.ru>
2019-02-14 13:40:04 +0500

    make dotOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
-
-$(call inherit-product, device/samsung/hltekor/full_hlte.mk)
-
-PRODUCT_DEVICE := hltekor
-PRODUCT_NAME := dot_hltekor
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/dot_hltekor.mk
